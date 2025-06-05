bc0557468cc0600a4155a983135831ae927c82db
Nolan Chappuis <nolanchappuis@hotmail.com>
2025-06-05 10:09:20 +0200

    Add Menu text and begin menu effects

---
+++ Assets/Scripts/MenuEffects.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuEffects : MonoBehaviour
+{
+    public List<GameObject> prefabs; // List of prefabs to instantiate
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
